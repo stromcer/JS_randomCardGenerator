fdf98d2b1a13b0f6d9584f9d692a5b7e41156e85
stromcer <dannymm12@hotmail.com>
2023-02-15 20:58:22 +0100

    POO card base-generator

--- src/app.js
+++ src/app.js
@@ -4,43 +4,22 @@
 
 // import "./assets/img/rigo-baby.jpg";
 // import "./assets/img/4geeks.ico";
+import { AllCards } from "./assets/modules/AllCards.js";
 
-
-let cardFamilys = [`♦`,`♥`,`♠`,`♣`],
+let cardFamilys = [`♦`,`♥`,`♠`,`♣`];
 let cardValues =  ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
 
 let $ = element => document.querySelector(element);
 
 
-
-
-function getRandomCard(){
-  let cardValue = getRandomIndex(cardValues);
-  let cardFamily =  getRandomIndex(cardFamilys);
-  return [cardValue,cardFamily]
-}
-
-
-function getRandomIndex(array){
-  return array[Math.floor(Math.random()*(array.length-1))]
-}
-
-
-
-
-
 function handleNewCardButton(){
-  let [newCard, newValue] = getRandomCard();
-  $("#cardValue").innerHTML = newCard;
-  $("#familyBot").innerHTML = newValue;
-  $("#familyTop").innerHTML = newValue;
+  AllCards.newCard();
 }
 
-$("#getNewCard").addEventListener(`click`,handleNewCardButton)
+
 
 
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  $("#getNewCard").addEventListener(`click`,handleNewCardButton);
 };
--- src/assets/modules/AllCards.js
+++ src/assets/modules/AllCards.js
@@ -1,5 +1,5 @@
 export let AllCards ={
-    values:[1,2,3,4,5,6,7,8,9,10,11,12,13],
+    values:["A","two","three","four","five","six","seven","eight","nine","ten","K","Q","K"],
     falmilys:[`♦`,`♥`,`♠`,`♣`],
     
     
@@ -9,9 +9,84 @@ export let AllCards ={
     newFamily:function(){
         return this.falmilys[Math.floor(Math.random()*(this.falmilys.length-1))]
     },
+    getColorCard:function(family){
+        if(family == `♥`|| family== `♦`){
+            document.getElementById("randomCardDiv").className ="text-danger";
+            return
+        }
+        document.getElementById("randomCardDiv").className ="text-black";
+    },
+
     newCard:function(){
-        let newCardValues = [this.newFamily(),this.newValue()]
+        const nuevaCarta = document.createElement("div");
+        let [newCardValue, newCardFamily] = [this.newValue(),this.newFamily()];
+        this.getOutside();
+        this.cardBodies[newCardValue]? this.cardBodies[newCardValue](newCardFamily) : this.cardBodies.figure(newCardFamily,newCardValue) ;
+        this.getColorCard(newCardFamily);
     },
     
+    getOutside:function(){
+        document.getElementById("randomCardDiv").innerHTML=`<div class="card mx-auto d-flex flex-row" >
+        <div id="familyTop"  class="card-top ms-2">         
+        </div>
+        <div id="cardBody"  class="card-body d-flex flex-column m-auto">
+        </div>
+        <div id="familyBot" class="card-bot me-2">
+        </div>  
+      </div>`;
+    },
+
+    cardBodies:{
+        figure:function(cardFamily, cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardFamily}${cardValue}</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="figureCard m-auto">${cardValue == "A"? cardFamily:cardValue}</div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardFamily}${cardValue}</div>`;
+        },
+        two:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}2</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="twoCard ">${cardValue}</div>  <div class="twoCard rotate180">${cardValue}</div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}2</div>`;
+        },
+        three:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}3</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="threeCard ">${cardValue}</div> <div class="threeCard ">${cardValue}</div> <div class="threeCard rotate180">${cardValue}</div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}3</div>`;
+        },
+        four:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}4</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="d-flex d-row fourCard "><div >${cardValue}</div> <div >${cardValue}</div> </div><div class="d-flex d-row fourCard"><div class="rotate180">${cardValue}</div> <div class="rotate180">${cardValue}</div> </div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}4</div>`;
+        },
+        five:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}5</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="d-flex d-row fiveCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row fiveCard "><div class="">${cardValue}</div></div><div class="d-flex d-row fiveCard"><div class="rotate180">${cardValue}</div><div class="rotate180">${cardValue}</div></div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}5</div>`;
+        },
+        six:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}6</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="d-flex d-row sixCard "> <div class="">${cardValue}</div> <div class="">${cardValue}</div></div><div class="d-flex d-row sixCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row sixCard"><div class="rotate180">${cardValue}</div><div class="rotate180">${cardValue}</div></div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}6</div>`;
+        },
+        seven:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}7</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="d-flex d-row sevenCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row sevenCard "><div class="">${cardValue}</div></div><div class="d-flex d-row sevenCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row sevenCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}7</div>`;
+        },
+        eight:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}8</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="d-flex d-row eightCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row eightCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row eightCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row eightCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}8</div>`;
+        },
+        nine:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}9</div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="d-flex d-row nineCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row nineCard "><div class="">${cardValue}</div></div><div class="d-flex d-row nineCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row nineCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row nineCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}9</div>`;
+        },
+        ten:function(cardValue){
+            document.getElementById("familyTop").innerHTML= `<div class="text-start h1">${cardValue}<div class="d-flex flex-row">10</div></div>`;
+            document.getElementById("cardBody").innerHTML= `<div class="d-flex d-row tenCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row tenCard "><div class="">${cardValue}</div></div><div class="d-flex d-row tenCard "><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row tenCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div><div class="d-flex d-row tenCard rotate180"><div class="">${cardValue}</div></div><div class="d-flex d-row tenCard rotate180"><div class="">${cardValue}</div><div class="">${cardValue}</div></div>`;
+            document.getElementById("familyBot").innerHTML= `<div class="text-start h1">${cardValue}<div class="d-flex flex-row">10</div></div>`;
+        }
+    }
 
 };
