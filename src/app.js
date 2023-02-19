75c2388ef0ab69e375cc7f3df7b43a609a63edca
Daniel Manjabacas <dannymm12@hotmail.com>
2023-02-19 11:05:17 +0000

    algunos ajustes para que vuelva a funcionar correctamente en gitpod

--- src/app.js
+++ src/app.js
@@ -1,31 +1,27 @@
-// /* eslint-disable */
-// import "bootstrap";
-// import "./style.css";
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 import { AllCards } from "./assets/modules/AllCards.js";
 
-let cardFamilys = [`♦`,`♥`,`♠`,`♣`];
-let cardValues =  ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+let cardFamilys = [`♦`, `♥`, `♠`, `♣`];
+let cardValues = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 let $ = element => document.querySelector(element);
 
-
-function handleNewCardButton(){
-  let element = document.createElement('div');
-  $('#randomCardDiv').append(AllCards.newCard(element))
+function handleNewCardButton() {
+  let element = document.createElement("div");
+  $("#randomCardDiv").append(AllCards.newCard(element));
 }
 
-function handleShuffleDeck(){
-  $('#randomCardDiv').innerHTML = ""
+function handleShuffleDeck() {
+  $("#randomCardDiv").innerHTML = "";
   AllCards.shuffleDeck();
 }
 
-
-
-
 window.onload = function() {
-  $("#shuffleDeck").addEventListener(`click`,handleShuffleDeck);
-  $("#getNewCard").addEventListener(`click`,handleNewCardButton);
+  $("#shuffleDeck").addEventListener(`click`, handleShuffleDeck);
+  $("#getNewCard").addEventListener(`click`, handleNewCardButton);
 };
